65a4accc54f6cef810ff7d32638eedf3a3ac33b6
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-01-25 17:57:10 -0300

    Creating front-end with Dependency Inject, Event Delegation and Builders using socket.io

--- public/pages/room/src/business.js
+++ public/pages/room/src/business.js
@@ -1,9 +1,13 @@
 class Business {
-  constructor({ room, media, view }) {
+  constructor({ room, media, view, socketBuilder }) {
     this.room = room;
     this.media = media;
     this.view = view;
 
+    this.socketBuilder = socketBuilder
+      .setOnUserConnected(this.onUserConnected())
+      .build();
+    this.socketBuilder.emit('join-room', this.room, 'teste01');
     this.currentStream = {};
   }
 
@@ -25,5 +29,11 @@ class Business {
       stream,
       isCurrent
     });
+  }
+
+  onUserConnected = function() {
+    return userId => {
+      console.log('user connected', userId);
+    }
   }
 }
